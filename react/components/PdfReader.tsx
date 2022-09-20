221fb3ed2c8057e3b4498342be6cf68ff7877534
JeferProgramer <jefer.talento.11@gmail.com>
2022-09-20 18:33:17 -0500

    finish component pdf reader

---
+++ react/components/PdfReader.tsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react'
+import {useCssHandles} from "vtex.css-handles"
+import "./styles.css"
+type Props = {
+  pdfUrl: string,
+  width: number,
+  height: number
+}
+
+const PdfReader = ({pdfUrl, width, height}: Props) => {
+
+  const CSS_HANDLES = ["container"]
+  const handles = useCssHandles(CSS_HANDLES)
+
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  },[])
+
+  return(
+    mounted && (
+      <div className={handles.container}>
+        <object data={pdfUrl} type="application/pdf" width={width} height={height}>
+          <iframe title='PDF' src={pdfUrl} width={width} height={height}>
+            <p>Este navegador no soporta PDF!</p>
+          </iframe>
+        </object>
+      </div>
+    )
+  )
+}
+
+export default PdfReader
