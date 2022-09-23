e9c9b1b764736371c8d9cf5ea07217bbe50b9bea
JeferProgramer <jefer.talento.11@gmail.com>
2022-09-23 13:04:51 -0500

    feat add component responsive

--- react/components/PdfReader.tsx
+++ react/components/PdfReader.tsx
@@ -23,8 +23,8 @@ const PdfReader = ({pdfUrl, width, height}: Props) => {
   return(
     mounted && (
       <div className={handles.container}>
-        <object data={pdfUrl} type="application/pdf" width={width} height={height}>
-          <iframe title='PDF' src={pdfUrl} width={width} height={height}>
+        <object data={pdfUrl} type="application/pdf" width={ isMobile ? 50 : width} height={isMobile ? 50 :height}>
+          <iframe title='PDF' src={pdfUrl} width={isMobile ? 150 : width} height={isMobile ? 150 :height}>
             <p>Este navegador no soporta PDF!</p>
           </iframe>
         </object>
