db5554b9cb2edf4d79dcf62ea44c2d7390a41df6
JeferProgramer <jefer.talento.11@gmail.com>
2022-09-23 12:56:23 -0500

    import device detector for desing responsive

--- react/components/PdfReader.tsx
+++ react/components/PdfReader.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {useCssHandles} from "vtex.css-handles"
+import {useDevice} from "vtex.device-detector"
 import "./styles.css"
 type Props = {
   pdfUrl: string,
